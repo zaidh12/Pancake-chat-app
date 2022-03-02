cd32e4a18770e014b81a2fe6542f377a75f6abff
Zaidh <imrzai001@myuct.ac.za>
2022-03-02 22:12:03 +0200

    OOP integrated

--- UDPClient.py
+++ UDPClient.py
@@ -2,12 +2,65 @@
 
 import socket
 
+class UDPClient:
+	# UDP Client socket class
+
+	def __init__(this, host, port):
+		this.host = host
+		this.port = port
+		this.server_addr = (this.host, this.port)
+		this.user = None
+		
+
+	def create_user(this):
+		this.user = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		print("User created")
+	
+	def sending_to_server(this):
+		try: 
+
+			while True:
+				data = input("Enter a message: ")
+
+				# User leaves the chat
+				if data == "bye" or data == "Bye" or data == "BYE":
+					data = data.encode("utf-8")
+					this.user.sendto(data, this.server_addr)
+					print("You left the chat")
+					break 
+
+				data = data.encode("utf-8")
+				this.user.sendto(data, this.server_addr)
+
+				data, this.server_addr = this.user.recvfrom(2048)
+				data = data.decode("utf-8")
+				print(f"Server: {data} {this.server_addr}")
+
+		except OSError as error:
+			print(error)
+
+		finally:
+			this.user.close()
+
+def main():
+	# Start a chat
+	client_obj = UDPClient("127.0.0.1", 12000)
+	client_obj.create_user()
+	client_obj.sending_to_server()
+
+if __name__ == "__main__":
+		main()
+
+''''
+
 if __name__ == "__main__":
 
-	host = "127.0.0.1"
+	host = "172.22.30.84"
 	port = 12000
 	addr = (host, port)
+	id = 12
 
+	# setup a UDP socket at client side
 	client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 	while True:
@@ -25,4 +78,7 @@ if __name__ == "__main__":
 
 		data, addr = client.recvfrom(2048)
 		data = data.decode('utf-8')
-		print(f"Server: {data}")
+		print(f"Server: {data} {addr}")
+
+	client.close()
+'''
--- UDPServer.py
+++ UDPServer.py
@@ -1,15 +1,80 @@
-# UDP server
+#'127.0.0.1', 444# UDP server
 
-import socket 
+import socket
+
+class UDPServer:
+	# UDP Server socket class
+
+	def __init__(this, host, port):
+		this.host = host 
+		this.port = port
+		#this.user_addr
+		this.server = None
+		
+	
+	def create_server(this):
+		this.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		this.server.bind((this.host, this.port))
+		print('Chat open')
+
+	def listen_for_user(this):
+		
+		try:
+				this.message_handling()
+			
+		except OSError as error:
+			print("No users found")
+
+		this.server.close()
+		
+
+	def message_handling(this):
+		while True:
+			data, user_addr = this.server.recvfrom(2048)
+			data = data.decode("utf-8")
+			
+			# User exits the chat
+			if data == "bye" or data == "Bye" or data == "BYE":
+				print("User left the chat")
+				break
+			
+			print(f"Client: {data} {user_addr}")
+			data = input("Enter a message: ")
+			data = data.encode("utf-8")
+			this.server.sendto(data, user_addr)
+
+		this.server.close()
+
+def main():
+	# Start a chat
+	server_obj = UDPServer("127.0.0.1", 12000)
+	server_obj.create_server()
+	server_obj.listen_for_user()
+	
 
 if __name__ == "__main__":
-	host = "127.0.0.1"
+	main()
+
+'''
+if __name__ == "__main__":
+	
+	host = ""
 	port = 12000
+#	socketList = []
+#	users = {}
 
+	# setup a server socket
 	server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1 )
 	server.bind((host, port))
+	
+#	socketList.append(server)
 
+	# server listening for connections
 	while True:
+
+	#	rList, wList, xList = select.select(socketList, [], [], 0)
+
 		data, addr = server.recvfrom(2048)
 		data = data.decode('utf-8')
 
@@ -17,11 +82,10 @@ if __name__ == "__main__":
 			print("User left the chat")
 			break
 
-		print(f"Client: {data}")
-
-		data = input("Enter a reply")
-		#data = data.upper()
+		print(f"Client: {data} {addr}")
+		data = input("Enter a reply: ")
 		data = data.encode('utf-8')
 		server.sendto(data, addr)
 
-	server.close()
+	server.close()'''
+
