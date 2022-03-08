dda7d728d70cfcbabc032645212267407b7b4422
RaniaDramat <92520983+RaniaDramat@users.noreply.github.com>
2022-03-08 09:40:44 +0200

    - Messages are sent to everyone except the sender

--- UDPClient.py
+++ UDPClient.py
@@ -1,13 +1,12 @@
 # UDP Client 
 
-from http import client
 import socket
 import threading
 
 
 class UDPClient:
 	# UDP Client socket class
-	# constuctor
+	# Constuctor
 	def __init__(self, host, port):
 		self.server_addr = (host, port)
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -16,6 +15,7 @@ class UDPClient:
 		name = input("Enter your name first (beginning with '@'):")
 		self.socket.sendto(name.encode(), self.server_addr)
 
+		#Threads - client's can send and receive
 		send_thread= threading.Thread(target=self.send)
 		receive_thread=threading.Thread(target=self.receive)
 
@@ -28,72 +28,42 @@ class UDPClient:
 			#Prompt user to enter message
 			message = input("")
 
-			# User leaves the chat
+			#User leaves the chat
 			if message == "bye" or message == "Bye" or message == "BYE":
 				message = message.encode("utf-8")
-				self.socket.sendto(message, self.server_addr) #Send encoded msg to server - host, port
+				self.socket.sendto(message, self.server_addr) 
 				print("You left the chat")
 				break 
-
-			message = message.encode("utf-8") #Send msg to server
+			
+			#Send message to the server
+			message = message.encode("utf-8") 
 			self.socket.sendto(message, self.server_addr)
 
 	
 	def receive(self):
-		#self.send()
 		while True:
-			#self.send()
 			try:
 				message, server_addr = self.socket.recvfrom(2048)
 				message = message.decode()
 
+				#If it is not the 'SENT' notification, then the username will be displayed with received message
+				if message != ">> SENT":
+					index_of_colon = (message.index(":")) + 2
+					username = message[index_of_colon:]
+					
+					if username.startswith('@'):
+						print(f'{username} HAS JOINED THE CHAT') # @.. HAS JOINED THE CHAT
 
-				if message[7:].startswith('@'):
-					print(f'{message[7:]} HAS JOINED THE CHAT')
+					else:
+						print(f'{message}')						# @.. : Hello
 
 				else:
 					print(f'{message}')
+
 			except:
 				pass
 
 
 
 client_obj = UDPClient("127.0.0.1", 12000)
-#print("You have joined the chat \nEnter your name first")
 
-		
-
-
-
-
-''''
-
-if __name__ == "__main__":
-
-	host = "172.22.30.84"
-	port = 12000
-	addr = (host, port)
-	id = 12
-
-	# setup a UDP socket at client side
-	client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-	while True:
-		data = input("Enter a message:")
-
-		if data == "bye" or data == "Bye":
-			data = data.encode('utf-8')
-			client.sendto(data, addr)
-
-			print ('You have left the chat.')
-			break
-
-		data = data.encode('utf-8')
-		client.sendto(data, addr)
-
-		data, addr = client.recvfrom(2048)
-		data = data.decode('utf-8')
-		print(f"Server: {data} {addr}")
-
-	client.close()
-'''
--- UDPServer.py
+++ UDPServer.py
@@ -8,49 +8,60 @@ port=12000
 
 server_soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_soc.bind((host, port))
-user_list = []
+
+user_list = [] #List of active client's addresses
+names = [] #List for to store the names of users - '@...'
 
 msg_log=[]
-port_and_msg=""
+name_and_msg=""
 joined_msg = "WELCOME"
 
 #Receive message from client and then broadcasts the message to all active clients.
-def receive_from_user():
+def receive_from_client():
 		
 	try:
 		while True:
 			#Message and address received from client
 			data, user_addr = server_soc.recvfrom(2048) 
 			data=data.decode()
-
-			#Add all messages to a log - historical tracking
-			port_and_msg= str(user_addr[1]) + ": " + data
-			msg_log.append(port_and_msg)
-			print(msg_log)
 			
-			#If address not already present, then add the address
+
+			#Message received from client, a notification is sent back to the client
+			notif = ">> SENT"
+			server_soc.sendto(notif.encode(), user_addr)
+
+			#If address not already present in the list, then add the address. 
+			#Add name to list
 			if user_addr not in user_list: 
 				user_list.append(user_addr)
+				
+				if data.startswith("@"):
+					names.append(data)
+
+
+			#Find postion of address in list. 
+			index = user_list.index(user_addr) 
+
+			#Corresp username stored in same position in names.
+			name_and_msg= str(names[index]) + ": " + data
+			msg_log.append(name_and_msg)
+			print(msg_log)
 
 			print(f'\nReceived message: {data} from {user_addr}')
 			print(user_list)
 
-			#Notification
-			sent_notif = ">> SENT"
-			server_soc.sendto(sent_notif.encode(), user_addr)
-
 			#Broadcasts message received to all active clients except one who sent the message
-			for user_addr in user_list:
-				if str(user_addr[1]) != port_and_msg[0:5]:
-					server_soc.sendto(port_and_msg.encode(), user_addr)
-
+			#Port numbers are compared.
+			for addr in user_list:
+				if str(addr[1]) != str(user_addr[1]):
+					server_soc.sendto(name_and_msg.encode(), addr)
 			
 	except OSError as error:
 		print("No users found")
 
 
 print('Chat open')
-receive_thread=threading.Thread(target=receive_from_user)
+receive_thread=threading.Thread(target=receive_from_client)
 receive_thread.start()
 
 
